f2555e8b5da6ee6ee4e4551c5d056a9c1d055c12
Dynaste <maximemary.pro@gmail.com>
2021-05-03 16:55:24 +0200

    link created

--- Day1/rpg/project.py
+++ Day1/rpg/project.py
@@ -14,45 +14,82 @@
 
 import csv
 
-with open('ressources/bosses.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    print("-------------------------------")
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            # print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            print(f'{row[1]} has {row[2]} HP and {row[4]} STR')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+class Players:
+    def init(self, name, hp, strength, rarity):
+        self.name = name
+        self.hp = hp
+        self.strength = strength
+        self.rarity = rarity
 
+# create Link character
 with open('ressources/players.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
-    print("-------------------------------")
     for row in csv_reader:
         if line_count == 0:
-
-            print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
-            # print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            print(f'{row[1]} has {row[2]} HP and {row[4]} STR')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+            if row[1] == "Link":
+                link = Players()
+                link.init(f'{row[1]}', f'{row[2]}', f'{row[3]}', f'{row[12]}')      
 
-with open('ressources/enemies.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    print("-------------------------------")
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            # print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            print(f'{row[1]} has {row[2]} HP and {row[4]} STR')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# with open('ressources/bosses.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     print("-------------------------------")
+#     for row in csv_reader:
+#         if line_count == 0:
+#             print(f'Column names are {", ".join(row)}')
+#             line_count += 1
+#         else:
+#             # print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+#             print(f'{row[1]} has {row[2]} HP and {row[4]} STR')
+#             line_count += 1
+#     print(f'Processed {line_count} lines.')
+
+
+
+# with open('ressources/players.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     print("-------------------------------")
+#     for row in csv_reader:
+#         if line_count == 0:
+#             print(f'Column names are {", ".join(row)}')
+#             line_count += 1
+#         else:
+#             player = Players()
+#             # print(row[1], row[2], row[3], row[12])
+#             player.init(row[1], row[2], row[3], row[12])
+#             print(f'{row[1]} has {row[2]} HP and {row[4]} STR')
+#             line_count += 1
+#     print(f'Processed {line_count} lines.')
+
+
+# with open('ressources/enemies.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     print("-------------------------------")
+#     for row in csv_reader:
+#         if line_count == 0:
+#             print(f'Column names are {", ".join(row)}')
+#             line_count += 1
+#         else:
+#             # print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+#             print(f'{row[1]} has {row[2]} HP and {row[4]} STR, this monster has a rarity of: {row[12]}')
+#             line_count += 1
+#     print(f'Processed {line_count} lines.')
